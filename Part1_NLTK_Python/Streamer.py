23e4b78f6fb6a90899fab6cc6cc9d9dc30940391
vladi7 <dubrovenski.v@gmail.com>
2020-02-15 10:42:28 -0600

    edited the file

--- Part1_NLTK_Python/Streamer.py
+++ Part1_NLTK_Python/Streamer.py
@@ -1,9 +1,9 @@
 #From Tweepy
 import tweepy
-access_token = "2822717651-AuHq6xAFkU56Cy2qKhtNTtkm1oERL2DTkLywlYM"
-access_token_secret = "6Lbx5l0MLY3WpP75xDiv31KOAOtum3Wnxgr7tGdahef7E"
-consumer_key = "SBwjKORZPuflTIyfXYkxlug9e"
-consumer_secret = "d5hMfDWOPrzwtfoQUEUIJXun19Ie5rkK0s2egSoy2c5CFPfrw8"
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
 
 
 class MyListener(tweepy.streaming.StreamListener):
